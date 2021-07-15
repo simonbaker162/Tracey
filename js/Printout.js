40698c63125e602a97d3059637c7e491bf49e08b
Simon Baker <simonbaker162@gmail.com>
2021-07-15 17:54:41 +0100

    refactor Printout class methods

--- js/Printout.js
+++ js/Printout.js
@@ -5,80 +5,85 @@ export class Printout {
 		this.printArea = document.getElementById("printArea");
 	}
 
+	promptPrint() {
+		window.print();
+	}
+
+	clearPrintArea() {
+		this.printArea.innerHTML = "";
+	}
+
 	generateMarkup() {
 		for (let i = 1; i <= this.numRows; i++) {
 			const name = document.getElementById(`${this.type}Name${i}`).value;
 			const size = document.getElementById(`${this.type}Size${i}`).value;
 			const repeats = document.getElementById(`${this.type}Repeats${i}`).value;
 
-			const formatPages = () => {
-				const pElements = this.printArea.getElementsByClassName(`${size} para-print ${name}-${i}`);
-				const childFirstRepeat = pElements[0];
-				const childLastRepeat = pElements[repeats - 1];
-				const personalisation = document.createElement("h2");
-				personalisation.innerText = `${name}'s tracing activity!`;
-				personalisation.classList.add("personalise");
-				childFirstRepeat.insertAdjacentElement("beforebegin", personalisation);
-				childLastRepeat.style.pageBreakAfter = "always";
-			};
-
-			switch (this.type) {
-				case "names":
-					for (let j = 1; j <= repeats; j++) {
-						printArea.insertAdjacentHTML("beforeend", `<p class="${size} para-print ${name}-${i}">${name}</p>`);
-					}
-					break;
-				case "words":
-					const word = document.getElementById(`wordsWord${i}`).value;
-					printArea.insertAdjacentHTML("beforeend", `<p>${name}</p>`);
-					for (let j = 1; j <= repeats; j++) {
-						printArea.insertAdjacentHTML("beforeend", `<p class="${size} para-print ${name}-${i}">${word}</p>`);
-					}
-					break;
-				case "alphabet":
-					const selectedCase = document.getElementById(`alphabetCase${i}`).value;
-					printArea.insertAdjacentHTML("beforeend", `<p>${name}</p>`);
-					for (let j = 1; j <= repeats; j++) {
-						if (selectedCase === "upper") {
-							printArea.insertAdjacentHTML(
-								"beforeend",
-								`<p class="${size} para-print ${name}-${i}">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>`
-							);
-						} else {
-							printArea.insertAdjacentHTML(
-								"beforeend",
-								`<p class="${size} para-print ${name}-${i}">abcdefghijklmnopqrstuvwxyz</p>`
-							);
-						}
-					}
-					break;
-				case "numbers":
-					const selectedNumbers = document.getElementById(`numbersNumbers${i}`).value;
-					printArea.insertAdjacentHTML("beforeend", `<p>${name} ${name}-${i}</p>`);
-					for (let j = 1; j <= repeats; j++) {
-						if (selectedNumbers === "20") {
-							printArea.insertAdjacentHTML(
-								"beforeend",
-								`<p class="${size} para-print ${name}-${i}">1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20</p>`
-							);
-						} else {
-							printArea.insertAdjacentHTML(
-								"beforeend",
-								`<p class="${size} para-print ${name}-${i}">1 2 3 4 5 6 7 8 9 10</p>`
-							);
-						}
-					}
-			}
-			formatPages();
+			this.buildPrintoutOfType(this.type, i, name, size, repeats);
+			this.formatPages(i, name, size, repeats);
 		}
 	}
 
-	clearPrintArea() {
-		const printArea = document.getElementById("printArea");
-		printArea.innerHTML = "";
+	formatPages(i, name, size, repeats) {
+		const pElements = this.printArea.getElementsByClassName(`${size} para-print ${name}-${i}`);
+		const childFirstRepeat = pElements[0];
+		const childLastRepeat = pElements[repeats - 1];
+		const personalisation = document.createElement("h2");
+
+		personalisation.innerText = `${name}'s tracing activity!`;
+		personalisation.classList.add("personalise");
+
+		childFirstRepeat.insertAdjacentElement("beforebegin", personalisation);
+		childLastRepeat.classList.add("last-repeat");
 	}
 
-	promptPrint() {
-		window.print();
+	buildPrintoutOfType(type, i, name, size, repeats) {
+		switch (type) {
+			case "names":
+				for (let j = 1; j <= repeats; j++) {
+					this.printArea.insertAdjacentHTML("beforeend", `<p class="${size} para-print ${name}-${i}">${name}</p>`);
+				}
+				break;
+			case "words":
+				const word = document.getElementById(`wordsWord${i}`).value;
+				printArea.insertAdjacentHTML("beforeend", `<p>${name}</p>`);
+				for (let j = 1; j <= repeats; j++) {
+					this.printArea.insertAdjacentHTML("beforeend", `<p class="${size} para-print ${name}-${i}">${word}</p>`);
+				}
+				break;
+			case "alphabet":
+				const selectedCase = document.getElementById(`alphabetCase${i}`).value;
+				this.printArea.insertAdjacentHTML("beforeend", `<p>${name}</p>`);
+				for (let j = 1; j <= repeats; j++) {
+					if (selectedCase === "upper") {
+						this.printArea.insertAdjacentHTML(
+							"beforeend",
+							`<p class="${size} para-print ${name}-${i}">ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>`
+						);
+					} else {
+						this.printArea.insertAdjacentHTML(
+							"beforeend",
+							`<p class="${size} para-print ${name}-${i}">abcdefghijklmnopqrstuvwxyz</p>`
+						);
+					}
+				}
+				break;
+			case "numbers":
+				const selectedNumbers = document.getElementById(`numbersNumbers${i}`).value;
+				this.printArea.insertAdjacentHTML("beforeend", `<p>${name} ${name}-${i}</p>`);
+				for (let j = 1; j <= repeats; j++) {
+					if (selectedNumbers === "20") {
+						this.printArea.insertAdjacentHTML(
+							"beforeend",
+							`<p class="${size} para-print ${name}-${i}">1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20</p>`
+						);
+					} else {
+						this.printArea.insertAdjacentHTML(
+							"beforeend",
+							`<p class="${size} para-print ${name}-${i}">1 2 3 4 5 6 7 8 9 10</p>`
+						);
+					}
+				}
+		}
 	}
 }
